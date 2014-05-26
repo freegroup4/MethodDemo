e895f7e1159cc428a244a0b712d46d2e5072751c
freegroup4 <freegroup4@yopmail.com>
2014-05-26 20:47:35 +0800

    Final Commit
    Final

--- src/methoddemo/MethodDemo.java
+++ src/methoddemo/MethodDemo.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package methoddemo;
-
-/**
- *
- * @author Chen-USE
- */
 public class MethodDemo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("length = " + aMethod(7,3));
     }
-    
+
+    public static double aMethod(int m, int n) {
+        return Math.sqrt(m*m+n*n);
+    }
 }
